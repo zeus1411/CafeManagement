7e86aaa5f3f1392f65b7007a9e50484bc17c42b5
Zeus1411 <121225888+zeus1411@users.noreply.github.com>
2025-03-02 21:47:08 +0700

    MenuInterface

---
+++ QuanLiCaphe/Menu.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace QuanLiCaphe
+{
+    partial class Menu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dtaGridHoaDon = new System.Windows.Forms.DataGridView();
+            this.btThanhToan = new System.Windows.Forms.Button();
+            this.btXoa = new System.Windows.Forms.Button();
+            this.tbTongTien = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btChon = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.tbTenMon = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dtGridMenu = new System.Windows.Forms.DataGridView();
+            this.tbRank = new System.Windows.Forms.TextBox();
+            this.tbTenKhach = new System.Windows.Forms.TextBox();
+            this.btSuaThongTin = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dtaGridHoaDon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridMenu)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dtaGridHoaDon
+            // 
+            this.dtaGridHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtaGridHoaDon.Location = new System.Drawing.Point(1120, 202);
+            this.dtaGridHoaDon.Name = "dtaGridHoaDon";
+            this.dtaGridHoaDon.RowHeadersWidth = 82;
+            this.dtaGridHoaDon.RowTemplate.Height = 33;
+            this.dtaGridHoaDon.Size = new System.Drawing.Size(398, 419);
+            this.dtaGridHoaDon.TabIndex = 33;
+            // 
+            // btThanhToan
+            // 
+            this.btThanhToan.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btThanhToan.Location = new System.Drawing.Point(1284, 709);
+            this.btThanhToan.Name = "btThanhToan";
+            this.btThanhToan.Size = new System.Drawing.Size(234, 63);
+            this.btThanhToan.TabIndex = 32;
+            this.btThanhToan.Text = "Thanh Toán";
+            this.btThanhToan.UseVisualStyleBackColor = true;
+            // 
+            // btXoa
+            // 
+            this.btXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btXoa.Location = new System.Drawing.Point(1120, 709);
+            this.btXoa.Name = "btXoa";
+            this.btXoa.Size = new System.Drawing.Size(158, 63);
+            this.btXoa.TabIndex = 31;
+            this.btXoa.Text = "Xoá";
+            this.btXoa.UseVisualStyleBackColor = true;
+            // 
+            // tbTongTien
+            // 
+            this.tbTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTongTien.Location = new System.Drawing.Point(1281, 634);
+            this.tbTongTien.Name = "tbTongTien";
+            this.tbTongTien.Size = new System.Drawing.Size(237, 40);
+            this.tbTongTien.TabIndex = 30;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1115, 636);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(170, 37);
+            this.label6.TabIndex = 29;
+            this.label6.Text = "Tổng tiền:";
+            // 
+            // btChon
+            // 
+            this.btChon.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btChon.Location = new System.Drawing.Point(320, 695);
+            this.btChon.Name = "btChon";
+            this.btChon.Size = new System.Drawing.Size(283, 63);
+            this.btChon.TabIndex = 28;
+            this.btChon.Text = "Chọn";
+            this.btChon.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(862, 643);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 31);
+            this.numericUpDown1.TabIndex = 27;
+            // 
+            // tbTenMon
+            // 
+            this.tbTenMon.Location = new System.Drawing.Point(320, 643);
+            this.tbTenMon.Name = "tbTenMon";
+            this.tbTenMon.Size = new System.Drawing.Size(442, 31);
+            this.tbTenMon.TabIndex = 26;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(159, 637);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(160, 37);
+            this.label5.TabIndex = 25;
+            this.label5.Text = "Tên Món:";
+            // 
+            // dtGridMenu
+            // 
+            this.dtGridMenu.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtGridMenu.Location = new System.Drawing.Point(89, 202);
+            this.dtGridMenu.Name = "dtGridMenu";
+            this.dtGridMenu.RowHeadersWidth = 82;
+            this.dtGridMenu.RowTemplate.Height = 33;
+            this.dtGridMenu.Size = new System.Drawing.Size(963, 419);
+            this.dtGridMenu.TabIndex = 24;
+            // 
+            // tbRank
+            // 
+            this.tbRank.Location = new System.Drawing.Point(807, 68);
+            this.tbRank.Name = "tbRank";
+            this.tbRank.Size = new System.Drawing.Size(164, 31);
+            this.tbRank.TabIndex = 23;
+            // 
+            // tbTenKhach
+            // 
+            this.tbTenKhach.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbTenKhach.Location = new System.Drawing.Point(320, 66);
+            this.tbTenKhach.Name = "tbTenKhach";
+            this.tbTenKhach.Size = new System.Drawing.Size(339, 40);
+            this.tbTenKhach.TabIndex = 22;
+            // 
+            // btSuaThongTin
+            // 
+            this.btSuaThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSuaThongTin.Location = new System.Drawing.Point(320, 112);
+            this.btSuaThongTin.Name = "btSuaThongTin";
+            this.btSuaThongTin.Size = new System.Drawing.Size(339, 71);
+            this.btSuaThongTin.TabIndex = 21;
+            this.btSuaThongTin.Text = "Sửa Thông Tin";
+            this.btSuaThongTin.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1257, 85);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(151, 37);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "Hoá Đơn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(857, 221);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 25);
+            this.label3.TabIndex = 19;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(696, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 37);
+            this.label2.TabIndex = 18;
+            this.label2.Text = "Rank:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(133, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 37);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Tên Khách:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1624, 874);
+            this.Controls.Add(this.dtaGridHoaDon);
+            this.Controls.Add(this.btThanhToan);
+            this.Controls.Add(this.btXoa);
+            this.Controls.Add(this.tbTongTien);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btChon);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.tbTenMon);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtGridMenu);
+            this.Controls.Add(this.tbRank);
+            this.Controls.Add(this.tbTenKhach);
+            this.Controls.Add(this.btSuaThongTin);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dtaGridHoaDon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtGridMenu)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dtaGridHoaDon;
+        private System.Windows.Forms.Button btThanhToan;
+        private System.Windows.Forms.Button btXoa;
+        private System.Windows.Forms.TextBox tbTongTien;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btChon;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox tbTenMon;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dtGridMenu;
+        private System.Windows.Forms.TextBox tbRank;
+        private System.Windows.Forms.TextBox tbTenKhach;
+        private System.Windows.Forms.Button btSuaThongTin;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
